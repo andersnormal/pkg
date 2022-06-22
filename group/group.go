d4a209e9d84b3593c5aa0f846db31949bec1fd41
Sebastian Döll <sebastian@katallaxie.me>
2022-06-22 12:55:15 +0000

    fix: pass error from group run

--- group/group.go
+++ group/group.go
@@ -5,16 +5,20 @@ import (
 	"sync"
 )
 
-// GroupRun is an abstraction on WaitGroup to run multiple functions in parallel.
-// It mimics 'errgroup' to extend structs with functions to run in parallel.
+// GroupRun is an abstraction on WaitGroup to run multiple functions concurrently.
+// It mimics 'errgroup' to extend structs with functions to run concurrently with
+// a root context.
 type GroupRun struct {
 	wg sync.WaitGroup
+
+	errOnce sync.Once
+	err     error
 }
 
 // ResolverFunc ...
 type GroupFunc func(ctx context.Context) error
 
-// Run is creating a new go routine to run a function in parallel.
+// Run is creating a new go routine to run a function concurrently.
 func (g *GroupRun) Run(ctx context.Context, fn GroupFunc) {
 	g.wg.Add(1)
 
@@ -23,12 +27,16 @@ func (g *GroupRun) Run(ctx context.Context, fn GroupFunc) {
 
 		err := fn(ctx)
 		if err != nil {
-			return // ignoring errors for now
+			g.errOnce.Do(func() {
+				g.err = err
+			})
 		}
 	}()
 }
 
 // Wait is waiting for all go routines to finish.
-func (g *GroupRun) Wait() {
+func (g *GroupRun) Wait() error {
 	g.wg.Wait()
+
+	return g.err
 }
