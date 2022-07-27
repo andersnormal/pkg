2e3846d83b50df3fb4260ac51a217090179fd8bf
Sebastian Döll <sebastian@katallaxie.me>
2022-07-27 06:08:44 +0000

    feat: adding file not exists

--- utils/files/exists.go
+++ utils/files/exists.go
@@ -13,3 +13,17 @@ func FileExists(path string) (bool, error) {
 
 	return true, nil
 }
+
+// FileNotExists is testing if a file does not exists at a given path.
+func FileNotExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err != nil && !os.IsNotExist(err) {
+		return false, err
+	}
+
+	if err == nil {
+		return false, nil
+	}
+
+	return true, nil
+}
--- utils/files/exists_test.go
+++ utils/files/exists_test.go
@@ -30,3 +30,26 @@ func TestFileExists(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, false, ok)
 }
+
+func TestFileNotExists(t *testing.T) {
+	tempDir, err := os.MkdirTemp(os.TempDir(), "empty_test")
+	assert.NoError(t, err)
+
+	defer func() { _ = os.RemoveAll(tempDir) }()
+
+	path := strings.Join([]string{tempDir, "example.txt"}, "/")
+	f, err := os.Create(path)
+	assert.NoError(t, err)
+	f.Close()
+
+	ok, err := FileNotExists(path)
+	assert.NoError(t, err)
+	assert.Equal(t, false, ok)
+
+	err = os.Remove(path)
+	assert.NoError(t, err)
+
+	ok, err = FileNotExists(path)
+	assert.NoError(t, err)
+	assert.Equal(t, true, ok)
+}
